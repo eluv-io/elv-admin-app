e86e7808fbda7cf4ac0d8d4c0bf19105ff4b8bd0
Loren Winzeler <loren.winzeler@eluv.io>
2024-02-16 11:03:29 -0800

    Testing stub, removes json array

---
+++ testing/ActionsData_debug_stub.ts
@@ -0,0 +1,13 @@
+
+
+import { Actions } from "./columns"
+
+// DEBUG: JSON array
+
+ export async function getActionsData(): Promise<Actions[]> {
+  // Fetch data from authClient API here.
+  return [
+        
+
+        ]
+    }
